3ce816d7a1006ae9886919a55e155714952d79de
now.imad <150154951+PerkasaM@users.noreply.github.com>
2026-06-12 16:39:25 +0700

    update report tampilan, fix double docnumber, filter chanel

---
+++ run.bat
@@ -0,0 +1,25 @@
+@echo off
+title ETL Insentif Q1
+
+echo ======================================
+echo ETL INSENTIF Q1
+echo ======================================
+echo.
+
+python etl_insentif.py
+
+if %errorlevel% neq 0 (
+    echo.
+    echo ERROR TERJADI!
+    pause
+    exit /b 1
+)
+
+echo.
+echo ETL BERHASIL
+echo Membuka folder output...
+echo.
+
+explorer output
+
+pause
